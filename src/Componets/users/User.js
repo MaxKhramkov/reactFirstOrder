1081124623317bcd2c01b761981b954f8c2e20de
max <khramov87@gmail.com>
2021-09-12 23:40:59 +0300

    new commit

--- src/Componets/users/User.js
+++ src/Componets/users/User.js
@@ -1,22 +1,27 @@
 import React, { Fragment, Component } from 'react';
-import Spinner from '../layout/Spinner';
 import PropTypes from 'prop-types';
+import Spinner from '../layout/Spinner';
+import Repos from '../repos/Repos';
 import { Link } from 'react-router-dom';
 
-export class User extends Component {
+class User extends Component {
     componentDidMount() {
         this.props.getUser(this.props.match.params.login);
+        this.props.getUserRepos(this.props.match.params.login);
     }
 
     static propTypes = {
         loading: PropTypes.bool,
         user: PropTypes.object.isRequired,
+        repos: PropTypes.array.isRequired,
         getUser: PropTypes.func.isRequired,
+        getUserRepos: PropTypes.func.isRequired,
     }
 
     render() {
         const {
             name,
+            company,
             avatar_url,
             location,
             bio,
@@ -28,25 +33,24 @@ export class User extends Component {
             public_repos,
             public_gists,
             hireable,
-            company,
         } = this.props.user;
 
-        const { loading } = this.props;
-
-        if (loading) return <Spinner />;
+        const { loading, repos } = this.props;
+        if (loading) return <Spinner />
 
         return (
             <Fragment>
                 <Link to='/' className='btn btn-light'>
                     Back To Search
                 </Link>
-                Hireable: {' '}
-                {hireable ? (<i className="fas fa-check text-success" />) : (<i className="fas fa-times-circle text-danger" />)}
+                Hirable: {' '}
+                {hireable ? (<i className="fas fa-check text-success" />) :
+                    (<i className="fas fa-times-circle text-danger" />)}
                 <div className="card grid-2">
                     <div className="all-center">
                         <img
                             src={avatar_url}
-                            alt={name}
+                            alt="Avatar"
                             className="round-img"
                             style={{ width: '150px' }}
                         />
@@ -56,11 +60,15 @@ export class User extends Component {
                     <div>
                         {bio && (
                             <Fragment>
-                                <h3>Bio</h3>
+                                <h3>Bio:</h3>
                                 <p>{bio}</p>
                             </Fragment>
                         )}
-                        <a href={html_url} className="btn btn-dark m-1">Visit GitHub Profile</a>
+                        <a
+                            href={html_url}
+                            className="btn btn-dark m-1">
+                            Visit GitHub Profile
+                        </a>
                         <ul>
                             <li>
                                 {login && <Fragment>
@@ -74,18 +82,31 @@ export class User extends Component {
                             </li>
                             <li>
                                 {blog && <Fragment>
-                                    <strong>WebSite: </strong>{blog}
+                                    <strong>Website: </strong>{blog}
                                 </Fragment>}
                             </li>
                         </ul>
                     </div>
+
                 </div>
-                <div className='card text-center'>
-                    <div className="badge badge-primary">Followers: {followers}</div>
-                    <div className="badge badge-success">Following: {following}</div>
-                    <div className="badge badge-light">Public Repos: {public_repos}</div>
-                    <div className="badge badge-dark">PublicGists: {public_gists}</div>
+                <div
+                    className='card text-center'
+                    style={{ display: 'flex', justifyContent: "space-around" }}
+                >
+                    <div className='badge badge-primary'>
+                        Followers: {followers}
+                    </div>
+                    <div className='badge badge-success'>
+                        Following: {following}
+                    </div>
+                    <div className='badge badge-light'>
+                        Public Repos: {public_repos}
+                    </div>
+                    <div className='badge badge-dark'>
+                        Public Gists: {public_gists}
+                    </div>
                 </div>
+                <Repos repos={repos} />
             </Fragment>
         )
     }
